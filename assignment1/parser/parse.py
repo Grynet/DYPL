98f8034f5054a98f3a697c38230b6c5067b194a2
Fredrik Nystad <nystad.fredrik@gmail.com>
2015-04-23 15:33:59 +0200

    Adding parse.py It can parse the required arithmetic expression and all statements except the for loop

---
+++ assignment1/parser/parse.py
@@ -0,0 +1,63 @@
+from pyparsing import *
+import re
+
+constant = Word(nums)
+variable = Word(alphas)
+operand = constant ^ variable
+
+sign = oneOf('+ -')
+multOp = '*'
+addSubOp = oneOf('+ -')
+
+expression = operatorPrecedence(operand, 
+			[(sign, 1, opAssoc.RIGHT),
+			(multOp, 2, opAssoc.LEFT),
+			(addSubOp, 2, opAssoc.LEFT)]
+			)
+
+statement = Forward()
+penDown = Literal("pen down")
+penUp =  Literal("pen up")             
+moveForward = Literal("move forward")
+moveBackward = Literal("move backward")
+move = "move(" + expression + "," + expression +")" 
+turnCW = "turn cw("+ expression + ")"
+turnCCW = "turn ccw(" + expression + ")"
+put = "put(" + expression + "," + expression + "," + expression + ")"
+forLoop = "for " + variable + "=" + expression + "to" + expression + "do\n" + statement + Optional("\n" + statement) + "\nend"
+statement = penDown ^  penUp ^  moveForward ^  moveBackward ^  move ^  turnCW ^  turnCCW ^  put ^ forLoop
+
+
+arithmeticTest = ["9",
+		"1+2",
+		"3*5",
+		"2-2",
+		"(2+5) * 3",
+		"(3*5+1)+(2*2)*(a-1)",
+		"(a*b)-x*y"
+		]
+
+
+statementTest = ["pen down",
+		"pen up",
+		"move forward",
+		"move backward",
+		"move(10,45)",
+		"turn cw(90)",
+		"turn ccw(30)",
+		"put(10,50,45)",
+		# "for X=0 to 10 \ndo pen down\nend"
+		]
+		
+
+for t in arithmeticTest:
+	print("Expression: %s" % t)	
+	print("Parse: %s" % expression.parseString(t))
+	print 
+	
+print '##################\n'
+	
+for t in statementTest:
+	print("Statement: %s" % t)
+	print("Parse: %s" % statement.parseString(t))
+	print
