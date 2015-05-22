2b666bb9cc152f5a85eab508f8d1533bbe4a87cf
Fredrik Nystad <nystad.fredrik@gmail.com>
2015-05-22 16:13:19 +0200

    Array#select_first() && Array#select_all() working Error detection and 'Increasesing the usability missing'

--- assignment2/array_extension.rb
+++ assignment2/array_extension.rb
@@ -1,47 +1,88 @@
 class Array
 	def select_first(**args)
-		puts "Array: #{self}"
-		puts "Arguments: #{args} | Args type: #{args.class} | Args size: #{args.length}"
-		key,value = args.first
-		puts "Value type: #{value.class} Length: #{value.length}"
-				
-		case value
-			when Array
-				self.each do |e|
-					value.each do |i|
-						if e.send(key) == i
-							return e
-						end
-					end
-				end
-				exit
-			
-			when Hash
-				min = value.has_key?(:min) ? value[:min] : 0
-				max = value[:max]
-				
-				self.each do |e|
-					attr = e.send(key)
-					if attr >= min && attr <= max
-						return e
-					end
-				end
-						
-				
-				exit
-			
-			else 
-				self.each do |e|
-					if e.send(key) == value
-						return e				
-					end
-				end
-				return "No such element found"
+		num_args = args.length				
+		if num_args == 1
+			return get_first(args)
+		elsif num_args == 2
+			return get_first_interval(args)
+		else
+			return nil
 		end
-	end	
+	end
 	
-	def select_all(args)
-		puts "Unimplemented method"
-	end	
+	def get_first(**args)	
+		key, value = args.first		
+		if value.class == Array
+			return self.find{ |e| value.include?(e.send(key))}			
+		else
+			return self.find{ |e| e.send(key) == value}		
+		end
+	end
+	
+	def get_first_interval(**args)		
+		function = args[:name]
+		interval = args[:interval]
+		min = interval.has_key?(:min) ? interval[:min] : nil
+		max = interval[:max]
+		
+		self.each do |e|
+			value = e.send(function)
+			if !min.nil?
+				if value >= min && value <= max
+					return e
+				end
+			else 
+				if value <= max
+					return e
+				end
+			end
+		end		
+		return nil
+	end
+	
+	def select_all(**args)
+		num_args = args.length
+		if num_args == 1
+			return get_all(args)
+		elsif num_args == 2
+			return get_all_interval(args)
+		else
+			return nil
+		end
+	end
+	
+	def get_all(**args)
+		key, value = args.first
+		if value.class == Array
+			return self.find_all{ |e| value.include?(e.send(key))}			
+		else
+			return self.find_all{ |e| e.send(key) == value}		
+		end	
+	end
+	
+	def get_all_interval(**args)		
+		function = args[:name]
+		interval = args[:interval]
+		min = interval.has_key?(:min) ? interval[:min] : nil
+		max = interval[:max]
+		results = Array.new
+		
+		self.each do |e|
+			value = e.send(function)
+			if !min.nil?
+				if value >= min && value <= max
+					results.push(e)
+				end
+			else
+				if value <= max
+					results.push(e)
+				end
+			end
+		end		
+		return results
+	end
+	
 end
+
+
 
