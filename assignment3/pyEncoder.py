97e13e151902ee9cba351a9adb566db70548a0cc
Fredrik Nystad <nystad.fredrik@gmail.com>
2015-05-30 13:14:29 +0200

    pyEncoder.py Refractored and documented

--- assignment3/pyEncoder.py
+++ assignment3/pyEncoder.py
@@ -1,3 +1,10 @@
+'''
+pyEncoder.py is used to encode numbers provided in a file of numbers into words that are provided 
+by a file of words. The main method 
+
+'''
+
+
 DICT = {
 	"6":6, "k":7, "v":6, "7":7, "l":8, "a":5, "w":2, 
 	"8":8, "b":7, "m":5, "x":2, "9":9, "c":6, "y":3, 
@@ -7,41 +14,51 @@ DICT = {
 	"j":1
 }
 
-words = []
+words = [] #holds words that has been read in from file
+results = [] #holds the matching encodings for numbers
 
-results = []
 
-def wordToNumber(word):
-	return ''.join(str(DICT[e]) for e in list(word))
-
+#reads every line of a file containing words and adds them to words
 def readWordFile(filePath):
 	with open(filePath, 'r') as theFile:
 		return [word.rstrip('\n').lower() for word in theFile]
 
-def match(number, word):
-	return wordToNumber(word) == number
+#reads every line of a file containing numbers and return a list of those numbers
+def readNumberFile(filePath):
+	with open(filePath, 'r') as theFile:
+		return [number.rstrip('\n') for number in theFile]
 
+def wordToNumber(word):
+	return ''.join(str(DICT[e]) for e in list(word))
 
-def findMatches(number, wordFilePath):
-	global words
-	words = readWordFile(wordFilePath)
-	encode(number, "")	
+#saves a successful number to word encoding
+def addMatch(encoding):	
+	results.append("%s : %s" %(wordToNumber(encoding.replace(" ","")), encoding))
 
-def encode(number, encoding):
-	global words
-	global results
+#builds number encodings
+def buildEncoding(number, encoding):
+	for word in words:			
+		if len(word) <= len(number):
+			wordAsNumber = wordToNumber(word)
+			if number.startswith(wordAsNumber):
+				newEncoding =  (encoding + word) if encoding == "" else (encoding + " %s" % (word))
+				encode(number[len(word):], newEncoding)	
+
+#adds or builds number to word encodings
+def encode(number, encoding):		
 	if number == "":
-		results.append("%s : %s" %(wordToNumber(encoding.replace(" ","")), encoding))
+		addMatch(encoding)
 	else:
-		for word in words:			
-			if len(word) <= len(number):
-				wordAsNumber = wordToNumber(word)
-				if number.startswith(wordAsNumber):
-					if(encoding == ""):
-						newEncoding = encoding + word
-					else:
-						newEncoding = encoding + " %s" % (word)	
-					newNumber = number[len(word):]	
-					encode(newNumber, newEncoding)	
+		buildEncoding(number, encoding)
 
+#find matchings word encodings for the numbers in the number and word files provided 
+def findMatches(numberFilePath, wordFilePath):
+	global words
+	numbers = readNumberFile(numberFilePath)
+	words = readWordFile(wordFilePath)
 
+	for number in numbers:
+		encode(number, "")	
+
+	for result in results:
+		print result
